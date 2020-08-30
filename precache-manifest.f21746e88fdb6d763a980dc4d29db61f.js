16edf31be4b04ce2dbba7b2da4739eb73e2908a9
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2020-08-30 14:25:15 +0500

    Updates

---
+++ precache-manifest.f21746e88fdb6d763a980dc4d29db61f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e14935321e5bf692254e36acacaa2e",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "5745fcd19b24f917b3bb",
+    "url": "/Calculator/static/css/main.8500a3da.chunk.css"
+  },
+  {
+    "revision": "114bbaea14242877659e",
+    "url": "/Calculator/static/js/2.6912228e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.6912228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5745fcd19b24f917b3bb",
+    "url": "/Calculator/static/js/main.1a66e5ea.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
